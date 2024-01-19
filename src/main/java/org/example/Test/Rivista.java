aa7fd7847e9d041f958a79f11d641bedd3bfac8f
Emilio-Plances <emilio.plances@gmail.com>
2024-01-19 16:27:29 +0100

    Progetto senza import

--- src/main/java/org/example/Test/Rivista.java
+++ src/main/java/org/example/Test/Rivista.java
@@ -2,7 +2,6 @@ package org.example.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Rivista extends Contenuto {
     Periodicita periodicita;
--- src/main/java/org/example/Test/Tester.java
+++ src/main/java/org/example/Test/Tester.java
@@ -5,16 +5,12 @@ import java.io.IOException;
 public class Tester {
     public static void main(String[] args) {
         Archivio a=new Archivio();
-        String[]  c={"HORROR,FANTASCIENZA,HORROR"};
-        System.out.println(Libro.convertToGenere(c).toString());
 
         a.aggiungiLibro("Harry Potter e i doni della morte",2018,369,"J.K.Rowling",Genere.HORROR);
         a.aggiungiLibro("Harry Potter e la pietra filosofale",2000,369,"J.K.Rowling",
                 Genere.FANTASCIENZA,Genere.HORROR);
 
         a.aggiungiRivista("Cioè",2024,20,Periodicita.SETTIMANALE);
-
-
 
         try{
             a.export();
